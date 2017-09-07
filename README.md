5fddd88c69b361027d11e453b1fabb4cd52fdc51
Vijay Parne <vijay.parne@accesscfa.com>
2017-09-07 00:01:34 -0400

    API for user profile followers

---
+++ README.md
@@ -0,0 +1,24 @@
+#  API Endpoint
+
+ test_git_id = 7633377
+ https://l4w6dh6uqi.execute-api.us-east-2.amazonaws.com/dev/github/users/<test_git_id>
+ 
+# Languages and Libraries used
+python
+Flask 
+zappa
+AWS 
+
+# Deployment
+AWS LAMBDA
+
+
+## Todo (Time Constraint)
+Commenting the code
+Testing 
+Proper organizing 
+
+
+### General Items
+- Add cli flag to use local Sqlite DB
+- Fix swagger generation to include all urls for a resource not just the first in the list
---
+++ app.py
@@ -0,0 +1,95 @@
+from __future__ import (absolute_import, print_function)
+import requests
+import json
+
+
+
+from flask import Flask
+from flask import jsonify, request
+
+app = Flask(__name__)
+
+
+def response_builder(github_id):
+    response = {}
+    follower_profile_list = []
+
+    github_client = requests.get('https://api.github.com/user/' + github_id)
+
+    if github_client.status_code == 200:
+
+        user_data = json.loads(github_client.text)
+        followers = requests.get(user_data['followers_url'])
+
+
+        if len(json.loads(followers.text)) == 0:
+
+            return {'github_username': user_data['login'],
+                            'github_id': user_data['id'],
+                            'profile_followers': []
+                            }
+        else:
+
+            user_profile_followers = json.loads(followers.text)
+
+            response.update({'github_username': user_data['login'],
+                             'github_id': user_data['id'],
+                             'profile_followers': []
+                             })
+
+
+            for follower in user_profile_followers:
+
+                follower_profile = {'github_username': follower['login'],
+                                    'github_id': follower['id'],
+                                    'profile_followers': []
+                                    }
+
+                follower_profile_list.append(follower_profile)
+
+            response['profile_followers'] = follower_profile_list
+
+            return response
+
+    else:
+
+        if github_client.status_code == 404:
+            message = 'user not found'
+        else:
+
+            message = 'something went wrong'
+
+        return {'Error': {
+            'error_code': github_client.status_code,
+            'error_message': message
+        }}
+
+
+@app.route('/github/users/<string:github_id>')
+def followers(github_id):
+
+    if github_id is not None:
+        response = response_builder(github_id)
+
+        count = 0
+        inner_loop_count = 0
+        for follower in response['profile_followers']:
+
+            profile_follower_response = response_builder(str(follower['github_id']))
+            response['profile_followers'][count]['profile_followers'] = profile_follower_response['profile_followers']
+
+            for profile in profile_follower_response['profile_followers']:
+                follower_response = response_builder(str(profile['github_id']))
+
+                response['profile_followers'][count]['profile_followers'][inner_loop_count]['profile_followers'] = follower_response['profile_followers']
+                inner_loop_count = inner_loop_count + 1
+            count = count + 1
+        return jsonify(response)
+    else:
+        return jsonify({'Error': {
+                'error_code': 400,
+                'error_message': "Please include Github Id param in the request"
+            }})
+
+if __name__ == '__main__':
+    app.run(debug=True)
